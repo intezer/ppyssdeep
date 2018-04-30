4227cd5d30dfd3c0e13f46ab0d9c6ddfa3b5167c
Jonathan Abrahamy <jonathan@intezer.com>
2018-04-30 09:52:53 +0300

    Prepared the setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
+import os
+
 import setuptools
+
+
+def parse_requirements(requirement_list):
+    for requirement in requirement_list:
+        if not requirement.startswith('-r'):
+            yield requirement
+        else:
+            file_to_read = requirement.lstrip('-r ')
+            with open(os.path.join('requirements', file_to_read)) as req_file:
+                for in_requirement in parse_requirements(req_file.readlines()):
+                    yield in_requirement
+
+
+def get_requirements():
+    with open(os.path.join('requirements', 'all.txt'), 'r') as req_file:
+        return list(parse_requirements(req_file.readlines()))
+
 
 setuptools.setup(name='ppyssdeep',
                  version='0.0.1',
-                 description='pure python ssdeep hash.',
+                 description='Pure python ssdeep hash.',
                  url='https://github.com/intezer/ppyssdeep',
                  packages=['ppyssdeep'],
-                 requires=['numpy'])
+                 install_requires=get_requirements(),
+                 include_package_data=True)
